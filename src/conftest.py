8e1560b89a25a0d5508a06b6e7d40e511d3b40b0
karwa <karwatmateusz07@gmail.com>
2023-04-29 19:22:18 +0200

    Final commit with all tests passing

--- src/conftest.py
+++ src/conftest.py
@@ -12,62 +12,62 @@ from utilities.Logger import Logger
 import logging
 
 
-# log = Logger(logging.DEBUG)
+log = Logger(logging.DEBUG)
 
 
-# @pytest.fixture(scope="class", autouse=True)
-# def driver_setup(request, browser, headless_mode):
-#     print("\nSetting up browser")
-#     try:
-#         if browser == "chrome":
-#             log.info(f"Initializing {browser}, headless_mode={headless_mode}")
-#             driver = Chrome(
-#                 service=Service(ChromeDriverManager().install()),
-#                 options=headless_mode,
-#             )
-#     except UnboundLocalError:
-#         print("Please select browser")
-#         log.error("Browser not selected")
-#     else:
-#         driver.maximize_window()
-#         request.cls.driver = driver
-#         print("\nBrowser up and running")
-#         yield
-#         print("\nTesting finished \nClosing browser")
-#         driver.close()
+@pytest.fixture(scope="class", autouse=True)
+def driver_setup(request, browser, headless_mode):
+    print("\nSetting up browser")
+    try:
+        if browser == "chrome":
+            log.info(f"Initializing {browser}, headless_mode={headless_mode}")
+            driver = Chrome(
+                service=Service(ChromeDriverManager().install()),
+                options=headless_mode,
+            )
+    except UnboundLocalError:
+        print("Please select browser")
+        log.error("Browser not selected")
+    else:
+        driver.maximize_window()
+        request.cls.driver = driver
+        print("\nBrowser up and running")
+        yield
+        print("\nTesting finished \nClosing browser")
+        driver.close()
 
 
-# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     outcome = yield
-#     rep = outcome.get_result()
-#     setattr(item, "rep_" + rep.when, rep)
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
 
 
-# @pytest.fixture(scope="function", autouse=True)
-# def check_test_result(request):
-#     yield
-#     if request.node.rep_setup.failed:
-#         print("setting up env failed")
-#         log.error("Setting up env failed")
-#     else:
-#         log.info(
-#             f"Test execution for something like {request.node.name} {request.node.rep_call.outcome}, taking a screenshot"
-#         )
-#         driver = request.cls.driver
-#         file_name = f'screenshots/{request.node.name}_{datetime.today().strftime("%Y-%m-%d_%H-%M")}.png'
-#         take_screenshot(driver, file_name)
-#         attach_screenshot_to_report(file_name)
+@pytest.fixture(scope="function", autouse=True)
+def check_test_result(request):
+    yield
+    if request.node.rep_setup.failed:
+        print("setting up env failed")
+        log.error("Setting up env failed")
+    else:
+        log.info(
+            f"Test execution for something like {request.node.name} {request.node.rep_call.outcome}, taking a screenshot"
+        )
+        driver = request.cls.driver
+        file_name = f'screenshots/{request.node.name}_{datetime.today().strftime("%Y-%m-%d_%H-%M")}.png'
+        take_screenshot(driver, file_name)
+        attach_screenshot_to_report(file_name)
 
 
-# def take_screenshot(driver, file_name):
-#     driver.save_screenshot(file_name)
-#     log.info(f"Screenshot {file_name} taken")
+def take_screenshot(driver, file_name):
+    driver.save_screenshot(file_name)
+    log.info(f"Screenshot {file_name} taken")
 
 
-# def attach_screenshot_to_report(file_name):
-#     allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
-#     log.info(f"Screenshot {file_name} attached to the report")
+def attach_screenshot_to_report(file_name):
+    allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
+    log.info(f"Screenshot {file_name} attached to the report")
 
 
 def pytest_addoption(parser):
