02c71188c0d532983719d2f4c51b6b2f6637b504
karwa <karwatmateusz07@gmail.com>
2023-05-04 22:32:09 +0200

    refactor tests and add new one

--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -4,6 +4,7 @@ from utilities.BaseTestClass import BaseTestClass
 from pages.LoginPage import LoginPage
 from utilities.Logger import Logger
 import logging
+from selenium.common.exceptions import TimeoutException
 
 
 class TestLogin(BaseTestClass):
@@ -31,16 +32,25 @@ class TestLogin(BaseTestClass):
         )
 
     @pytest.mark.login
+    @pytest.mark.xfail(
+        raises=TimeoutException,
+        reason="User should not be able to log in with invalid credentials",
+    )
     @allure.title("Test to check login functionality with invalid credentials")
     def test_login_page_invalid_credentials(self):
-        # login_page.username_input(self.invalid_credentials[0])
-        # login_page.password_input(self.invalid_credentials[1])
-        # login_page.login_button_click()
         self.login_page.login_to_system(
             self.invalid_credentials[0], self.invalid_credentials[1]
         )
-        # assert login_page.is_user_logged(), "User logged in"
+        assert self.login_page.is_user_logged(), self.log.error(
+            f"User not logged in with credentials: username - {self.invalid_credentials[0]}, password - {self.invalid_credentials[1]}"
+        )
 
+    @pytest.mark.login
+    @allure.title("Test to check if error border is visible for invalid credentials")
+    def test_login_page_error_visible(self):
+        self.login_page.login_to_system(
+            self.invalid_credentials[0], self.invalid_credentials[1]
+        )
         assert self.login_page.is_error_border_visible(), self.log.error(
-            f"User logged in with credentials: username - {self.invalid_credentials[0]}, password - {self.invalid_credentials[1]}"
+            f"Border not visible, user logged in with credentials: username - {self.invalid_credentials[0]}, password - {self.invalid_credentials[1]}"
         )
